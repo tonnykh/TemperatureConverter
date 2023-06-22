7ff10266836bcfba56a64887e06c34e254f58955
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-22 20:00:29 +0530

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Khumbongmayum Tonny on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
