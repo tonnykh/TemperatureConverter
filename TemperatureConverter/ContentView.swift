3619e38c8220e8a93376574cb8c66bec0ffa2c90
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-22 21:25:52 +0530

    Add basic UI

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userInputNumber: Int = 0
+    @State private var convertTemperatureFrom: String = "Celcius"
+    @State private var convertTemperatureTo: String = "Fahrenheit"
+    
+    private var outputNumber: Double = 0
+    
+    let units: [String] = [
+        "Celcius", "Fahrenheit", "Kelvin"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter value", value: $userInputNumber, format: .number)
+                    
+                    Picker("Convert from", selection: $convertTemperatureFrom) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Convert from")
+                }
+                
+                Section {
+                    Picker("Converted to", selection: $convertTemperatureTo) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Text(outputNumber, format: .number)
+                } header: {
+                    Text("Converted to")
+                }
+
+            }
+            .navigationTitle(Text("Temperature Convert"))
         }
-        .padding()
     }
 }
 
